1550e57ea45bb1526b11f091df5c220d76099fa0
aprentix <raqueldiazchavez9@gmail.com>
2025-04-22 17:09:37 +0200

    feat: run file

--- src/run.py
+++ src/run.py
@@ -1,5 +1,9 @@
 from dataset import DatasetManager
 
-dataset_manager = DatasetManager()
-dataset_manager.read_anual_resume(0.042, "2015-01-01", "2020-01-01")
+def main():
+    dataset_manager = DatasetManager()
+    dataset_manager.read_anual_resume(0.042, "2015-01-01", "2020-01-01")
 
+
+if __name__ == "__main__":
+    main()
