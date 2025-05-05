faab07850e04ce5780f9b2895ace410e75c5c088
Leonardo Chen <leochenjin@gmail.com>
2025-05-06 00:43:42 +0200

    fix: fixed modules init implementation

--- pop/__init__.py
+++ pop/__init__.py
@@ -4,8 +4,4 @@ Portfolio Optimization Problem (POP) package
 Portfolio optimization is a fundamental challenge in investment management, focusing on allocating capital among assets to maximize returns while minimizing risk.
 This problem is crucial for both institutional investors managing billions of dollars and individuals growing their savings.
 
-Methods:
-    - runner: Run optimization problem
 """
-
-from .runner import runner
