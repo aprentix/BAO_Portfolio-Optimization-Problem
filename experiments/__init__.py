c7c9200f2f48403616ed1b99aca9037bda90d5c6
Leonardo Chen <leochenjin@gmail.com>
2025-05-15 15:19:40 +0200

    refactor: experiments data folder, not module

--- experiments/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Portfolio Optimization Problem (POP) experiments package
-
-Portfolio optimization is a fundamental challenge in investment management, focusing on allocating capital among assets to maximize returns while minimizing risk.
-This problem is crucial for both institutional investors managing billions of dollars and individuals growing their savings.
-"""
